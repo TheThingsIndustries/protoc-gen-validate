adf380c29e248aa00e6cb3260115a9e3b2bd705c
Roman Volosatovs <rvolosatovs@riseup.net>
2019-02-22 14:07:19 +0100

    templates/{go,gogo,goshared}: Support GoGo customtypes

--- templates/go/register.go
+++ templates/go/register.go
@@ -3,12 +3,12 @@ package golang
 import (
 	"text/template"
 
-	"github.com/lyft/protoc-gen-star"
 	"github.com/envoyproxy/protoc-gen-validate/templates/goshared"
+	pgsgo "github.com/lyft/protoc-gen-star/lang/go"
 )
 
 func Register(tpl *template.Template, params pgs.Parameters) {
-	goshared.Register(tpl, params)
+	goshared.Register(tpl, params, pgsgo.InitContext)
 	template.Must(tpl.Parse(fileTpl))
 	template.Must(tpl.New("required").Parse(requiredTpl))
 	template.Must(tpl.New("timestamp").Parse(timestampTpl))
