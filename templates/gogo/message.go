c97df826497e7191af77a2a1c9bc6e7ef6ea9448
Roman Volosatovs <rvolosatovs@riseup.net>
2019-02-22 14:08:06 +0100

    templates/goshared: Validate only specified fieldpaths

--- templates/gogo/message.go
+++ templates/gogo/message.go
@@ -7,17 +7,10 @@ const messageTpl = `
 	{{ if $r.GetSkip }}
 		// skipping validation for {{ $f.Name }}
 	{{ else }}
-	{
-		tmp := {{ accessor . }}
-		{{ if .Gogo.Nullable }}
-		if v, ok := interface{}(tmp).(interface{ Validate() error }); ok {
-		{{ else }}
-		if v, ok := interface{}(&tmp).(interface{ Validate() error }); ok {
-		{{ end }}
-			if err := v.Validate(); err != nil {
+		if v, ok := interface{}({{ accessor . }}).(interface{ ValidateFields(...string) error }); ok {
+			if err := v.ValidateFields(subs...); err != nil {
 				return {{ errCause . "err" "embedded message failed validation" }}
 			}
 		}
-	}
 	{{ end }}
 `
